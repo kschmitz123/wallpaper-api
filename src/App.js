fa609848e2c3bc154157fe4ccdda0c8da9d95c1f
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-27 15:33:27 +0100

    Add basic structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import ImagePreview from "./components/imagePreview";
 import "./global.css";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>Get Random Image</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1603648913629-3b83ed3e6be9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        }
+        alt={"Decoration"}
+        author={"me"}
+      />
+    </main>
+  );
 }
 
 export default App;
