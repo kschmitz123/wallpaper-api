7e79ddc0861f9b224921cda8806ea2006292d2bb
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-27 15:41:05 +0100

    Style page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import ImagePreview from "./components/imagePreview";
+import "./App.css";
 import "./global.css";
 
 function App() {
   return (
     <main>
-      <button>Get Random Image</button>
+      <button className="randomBtn">Get Random Image</button>
       <ImagePreview
         src={
           "https://images.unsplash.com/photo-1603648913629-3b83ed3e6be9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
