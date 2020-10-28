242a68fbb567f825a2b2440b4dc73cf0730f3b36
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-28 09:37:36 +0100

    Add alt description

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import { getRandomImage } from "./api/getRandomImage";
 function App() {
   const [wallpaper, setWallpaper] = useState();
   const [author, setAuthor] = useState();
+  const [altText, setAltText] = useState();
 
   async function loadImages() {
     const images = await getRandomImage();
     setWallpaper(images.urls.regular);
     setAuthor(images.user.username);
+    setAltText(images.alt_description);
   }
   return (
     <main>
       <button onClick={() => loadImages()} className="randomBtn">
         Get Random Image
       </button>
-      <ImagePreview src={wallpaper} author={author} />
+      <ImagePreview src={wallpaper} alt={altText} author={author} />
     </main>
   );
 }
