ef6658685e0009511bff5e5d8abaf86a4859b35b
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-28 16:01:34 +0100

    Add test file

---
+++ src/components/FavoriteImage.test.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import FavoriteImage from "./FavoriteImage";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<FavoriteImage id="AZTc9bXx7ko" />, div);
+});
