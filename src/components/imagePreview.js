bc9aacceb96c2d4867067f888598a79ddedee2f2
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-28 12:27:05 +0100

    Add favorites function

--- src/components/imagePreview.js
+++ src/components/imagePreview.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./imagePreview.css";
 
-export default function ImagePreview({ src, alt, author }) {
+export default function ImagePreview({ src, alt, author, id }) {
+  function addFavorites(id) {
+    const oldFavorites = JSON.parse(localStorage.getItem("favorites") || "[]");
+    const newFavorites = [...oldFavorites, id];
+    localStorage.setItem("favorites", JSON.stringify(newFavorites));
+    return newFavorites;
+  }
   return (
     <div className="imageContainer">
+      <button onClick={() => addFavorites(id)}>🤍</button>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
